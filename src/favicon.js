feacb8f4af41141117b59c8771ad3a750538dcfb
Daniel Fernandes <57069381+dannywritescode@users.noreply.github.com>
2021-07-19 13:05:59 +0530

    set a random color based on the name
    the hue is generated using the name.
    this ensures that the same name always gets the same color.
    a randomSeed option is provided for different colors.
    the background can now also be a circle or square.

--- src/favicon.js
+++ src/favicon.js
@@ -2,10 +2,13 @@ function FaviconFallback(name, userOptions) {
     const options = {
         font: "128px sans-serif",
         size: 256,
+        circle: false,
+        randomSeed: 0,
         ...userOptions,
     };
 
     const initials = getInitials(name);
+    const hue = getHue(name, options.randomSeed);
 
     const canvas = document.createElement("canvas");
 
@@ -16,6 +19,12 @@ function FaviconFallback(name, userOptions) {
 
     const ctx = canvas.getContext("2d");
     ctx.scale(devicePixelRatio, devicePixelRatio);
+
+    if (options.circle) ctx.arc(canvasSize/2, canvasSize/2, canvasSize/2, 0, 2 * Math.PI, false);
+    else ctx.rect(0, 0, canvasSize, canvasSize);
+    ctx.fillStyle = `hsla(${hue}, 50%, 80%, 100%)`;
+    ctx.fill();
+
     ctx.font = options.font;
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
@@ -29,6 +38,7 @@ function FaviconFallback(name, userOptions) {
     const textY = (canvasSize - textHeight) / 2 + baselineOffset;
     const maxWidth = canvasSize * 0.9;
 
+    ctx.fillStyle = `hsla(${hue}, 50%, 50%, 100%)`;
     ctx.fillText(initials, textX, textY, maxWidth);
 
     return canvas.toDataURL("image/png");
@@ -38,4 +48,14 @@ function getInitials(name) {
     let initials = "";
     for (const word of name.split(" ")) initials += word[0];
     return initials;
+}
+
+function getHue(name, seed = 0) {
+    let number = 0;
+    for (let i = 0; i < name.length; i++) {
+        number += name.charCodeAt(i);
+        number *= name.charCodeAt(i);
+        number += seed;
+    }
+    return number % 360;
 }
